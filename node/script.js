aad7836c8cfa31ef2b5784ef9b0c07b6fa979abf
KayKozaronek <kozaronek@gmail.com>
2021-01-23 15:07:16 +0100

    adding some node and express startet files

---
+++ node/script.js
@@ -0,0 +1,8 @@
+const script2 = require("./script2.js")
+
+const a = script2.largeNumber;
+const b = 1123;
+
+console.log(a+b);
+
+
---
+++ node/server.js
@@ -0,0 +1,25 @@
+const { response } = require("express");
+const express = require("express");
+
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.use(express.static(__dirname + "/public"))
+
+app.get("/", (req, res) => {
+    //console.log(req.query)
+    //console.log(req.body)
+    //console.log(req.headers)
+    //console.log(req.params)
+    res.status(404).send("Not found");
+});
+
+
+
+app.listen(3000);
+
+
+
